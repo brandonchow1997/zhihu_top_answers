893f607c9122148ad77d7fa2a3a873def4f303bc
Brandon <40913735+brandonchow1997@users.noreply.github.com>
2018-09-02 09:59:12 +0800

    selenium update

--- zhihu_topic_selenium/main.py
+++ zhihu_topic_selenium/main.py
@@ -5,7 +5,17 @@ import top_answers
 
 
 if __name__ == '__main__':
-    response = login.get_topic()
-    url_token = response[1]
-    total = response[0]
-    top_answers.top_answer(total, url_token)
+    print('-- 请选择 --')
+    print('-- 1 Chrome模式 --')
+    print('-- 2 无浏览器模式 --')
+    choose = input()
+    if choose == '1':
+        response = login.get_topic()
+        url_token = response[1]
+        total = response[0]
+        top_answers.top_answer(total, url_token)
+    if choose == '2':
+        response = login.get_topic()
+        url_token = response[1]
+        total = response[0]
+        top_answers.top_answer_opt(total, url_token)
--- zhihu_topic_selenium/top_answers.py
+++ zhihu_topic_selenium/top_answers.py
@@ -6,20 +6,35 @@ from selenium.webdriver.support.wait import WebDriverWait
 import time
 from lxml import etree
 
-# 声明浏览器对象初始化
-browser = webdriver.Chrome()
-wait = WebDriverWait(browser, 10)
-
 
 def top_answer(total, url_token):
+    # 声明浏览器对象初始化
+    browser = webdriver.Chrome()
+    wait = WebDriverWait(browser, 10)
+
     for i in range(1, total):
         url_top_answers = 'https://www.zhihu.com/topic/{url_token}/top-answers'.format(url_token=url_token[i-1])
         browser.get(url_top_answers)
-        get_answer_url()
+        get_answer_url(browser)
         time.sleep(2)
 
 
-def get_answer_url():
+def top_answer_opt(total, url_token):
+    # 创建chrome参数对象
+    opt = webdriver.ChromeOptions()
+    opt.set_headless()
+
+    # 创建chrome无界面对象
+    browser = webdriver.Chrome(options=opt)
+    wait = WebDriverWait(browser, 10)
+    for i in range(1, total):
+        url_top_answers = 'https://www.zhihu.com/topic/{url_token}/top-answers'.format(url_token=url_token[i-1])
+        browser.get(url_top_answers)
+        get_answer_url(browser)
+        time.sleep(2)
+
+
+def get_answer_url(browser):
     html = browser.page_source
     data = etree.HTML(html)
     items = data.xpath('//*[@id="TopicMain"]/div[3]/div/div/div')
@@ -28,12 +43,12 @@ def get_answer_url():
             url_content = item.xpath('./div/meta[3]/@content')
             print('=' * 60)
             print(url_content[0])
-            answer_content(url_content[0])
+            answer_content(url_content[0], browser)
         except Exception:
             pass
 
 
-def answer_content(url):
+def answer_content(url, browser):
     try:
         browser.get(url)
         html = browser.page_source
@@ -46,12 +61,17 @@ def answer_content(url):
         comments = data.xpath('//*[@id="root"]/div/main/div/div[2]/div[1]/div[2]/div/div/div[2]/div[3]/div[1]/button[1]/text()')
         contents = data.xpath('//*[@class="RichText ztext CopyrightRichText-richText"]/child::text()')
         ################################
-        print(question[0])
-        print(answerer[0], end='||')
-        print(answerer_info[0])
-        print(vote[0])
+        for que in question:
+            print(que)
+        for ans in answerer:
+            print(ans, end='||')
+        for ans_info in answerer_info:
+            print(ans_info)
+        for vote_num in vote:
+            print(vote_num)
+        for com in comments:
+            print(com)
         print('-' * 60)
-        print(comments[0])
         try:
             for content in contents:
                 print(content)
